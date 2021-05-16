9a21aff43883c647dc186afbd063f54b75e6b64c
robhaber <robhaber@wustl.edu>
2021-05-16 17:33:48 -0400

    fixed readme

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ The preferred way to run the code is in the Colab notebook provided on the websi
 
 If you choose to run it locally, you will need a GPU, and the following:
 
-```pip install gdown
+```
+pip install gdown
 gdown https://drive.google.com/uc?id=1_UwcQZg3d_3ZzI7TlEuXolpJTjqOe8Ii
 mv wav2lip_gan.pth Toon-talker/Wav2Lip/checkpoints/wav2lip_gan.pth
 pip install -r requirements.txt
 ```
 
 Proceed with:
-```export FLASK_APP=server.py
+```
+export FLASK_APP=server.py
 flask run
 ```
 
--- lipsync.py
+++ lipsync.py
@@ -18,8 +18,6 @@ def run():
     
 
     os.system("cd Wav2Lip && python3 inference.py --checkpoint_path checkpoints/wav2lip_gan.pth --face '../vid_out.mp4' --audio '../speech_out.wav' --pads 0 40 0 0")
- #   os.system("cd Wav2Lip && python3 inference.py --checkpoint_path  https://iiitaphyd-my.sharepoint.com/:u:/g/personal/radrabha_m_research_iiit_ac_in/EdjI7bZlgApMqsVoEUUXpLsBxqXbn5z8VTmoxp55YNDcIA?e=n9ljGW --face '../vid_out.mp4' --audio '../speech_out.wav' --pads 0 40 0 0")
-
     os.system("mv Wav2Lip/results/result_voice.mp4 static/output/video.mp4")
 
 run()
